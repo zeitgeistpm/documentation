bbc043c0b84f277d1a27fc935b84b156df3a7020
Logan Saether <X@logansaether.com>
2020-12-01 20:32:20 +0100

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  title: 'Zeitgeist Documentation',
+  tagline: 'Learn how to integrate with the Zeitgeist network.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'zeitgeistpm', 
+  projectName: 'docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Zeitgeist Documentation',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/Zeitgeist-trans.png',
+      },
+      items: [
+        {
+          href: 'https://github.com/zeitgeistpm',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/',
+            },
+            {
+              label: 'Zeitgeist SDK',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Website',
+              href: 'https://zeitgeist.pm',
+            },
+            {
+              label: 'Discord',
+              href: '#',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/zeitgeistpm',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'https://zeitgeist.pm/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/zeitgeistpm',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Zeitgeist PM`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/zeitgeistpm/docs/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
