1446d8de8d6732061679c3ef8ff65a6aec71fdcc
maltekliemann <mail@maltekliemann.com>
2021-10-31 23:31:26 +0100

    Add latex support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,6 @@
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 module.exports = {
   title: 'Zeitgeist Documentation',
   tagline: 'Learn how to integrate with the Zeitgeist network.',
@@ -84,11 +87,20 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/zeitgeistpm/docs/edit/main/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
+  ],
+  stylesheets: [
+    {
+        href: "https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css",
+        integrity: "sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc",
+        crossorigin: "anonymous",
+    },
   ],
 };
