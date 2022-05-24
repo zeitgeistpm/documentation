c75f6e44aa6477441114713a9b4d4b696f17ec4a
Whisker17 <yukaijun123456@163.com>
2022-05-24 10:56:21 +0800

    simplify the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,11 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/">
-            Zeitgeist Tutorial - 1min ⏱️
+            Click here to know more 
           </Link>
         </div>
       </div>
